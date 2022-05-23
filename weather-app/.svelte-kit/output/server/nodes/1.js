5b34e50d795cbb8435affce35a19c2186402040a
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-23 19:29:45 -0400

    Add weather app

---
+++ weather-app/.svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,6 @@
+import * as module from '../entries/fallbacks/error.svelte.js';
+
+export { module };
+export const entry = 'error.svelte-09d12047.js';
+export const js = ["error.svelte-09d12047.js","chunks/index-dadf112b.js"];
+export const css = [];
---
+++ weather-app/.svelte-kit/output/server/nodes/2.js
@@ -0,0 +1,6 @@
+import * as module from '../entries/pages/index.svelte.js';
+
+export { module };
+export const entry = 'pages/index.svelte-a760d40b.js';
+export const js = ["pages/index.svelte-a760d40b.js","chunks/index-dadf112b.js"];
+export const css = [];
