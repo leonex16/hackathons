c3fc8e026af366f00108683ae89320abd3d71065
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-29 19:54:00 -0400

    Update playwright config

--- weather-app/playwright.config.ts
+++ weather-app/playwright.config.ts
@@ -1,6 +1,15 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	testDir: './tests',
+	use: {
+		headless: true,
+		defaultBrowserType: 'chromium',
+		viewport: {
+			width: 375,
+			height: 667,
+		}
+	},
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 3000
