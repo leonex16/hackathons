808e4f47ef675b0d284f54d3f51a0e72698ee82f
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-29 15:07:45 -0400

    Refactor auto complete endpoint, split functiontality into functions and move types to independient file

--- weather-app/src/routes/api/auto-complete/index.ts
+++ weather-app/src/routes/api/auto-complete/index.ts
@@ -1,46 +1,59 @@
-import type { RequestEvent } from "@sveltejs/kit";
+import type { RequestEvent } from '@sveltejs/kit';
+import type { AutoComplete } from '@/src/routes/api/auto-complete/types';
 import countryCodes from '@/src/data/country-codes.json';
-
-interface AutoCompleteResponse {
-  id:      number;
-  name:    string;
-  region:  string;
-  country: string;
-  lat:     number;
-  lon:     number;
-  url:     string;
-}
 
 const URL = 'https://weatherapi-com.p.rapidapi.com';
 const options = {
-	headers: {
-		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
-		'X-RapidAPI-Key': 'fc6e05bb36msheefffdebdddd71bp153576jsn72eb5ac4b2fe'
-	}
+  headers: {
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+    'X-RapidAPI-Key': 'fc6e05bb36msheefffdebdddd71bp153576jsn72eb5ac4b2fe',
+  },
 };
 
-const parseData = ( data: AutoCompleteResponse[] ) => {
-  return data.slice(0,5).map(({ name, region, country }) => {
+const filterData = (data: AutoComplete[], ids: string) => {
+  const MAX_ITEMS = 5;
+  const currentIds = ids.split('$');
+
+  return data
+    .filter(({ lat, lon }) => {
+      const id = `${lat};${lon}`;
+      const isRenderizedItem = currentIds.includes(id);
+
+      return !isRenderizedItem;
+    })
+    .slice(0, MAX_ITEMS);
+};
+
+const mapData = (data: AutoComplete[]) => {
+  return data.map(({ country, lat, lon, name, region }) => {
     const foundCountry = countryCodes.find(({ country: c }) => c === country.toLowerCase());
 
     return {
+      id: `${lat};${lon}`,
       name,
       region,
       country,
-      src: `https://countryflagsapi.com/svg/${foundCountry?.numeric}`
-    }
-  })
-}
+      src: `https://countryflagsapi.com/svg/${foundCountry?.numeric}`,
+    };
+  });
+};
 
-export async function get({url}: RequestEvent) {
+const parseData = (data: AutoComplete[], ids: string) => {
+  const filteredData = filterData(data, ids);
+
+  return mapData(filteredData);
+};
+
+export async function get({ url }: RequestEvent) {
   const query = url.searchParams.get('q');
-  const response = await fetch(`${URL}/search.json?q=${query}`, {...options, method: 'GET'});
-  const data: AutoCompleteResponse[] = await response.json();
+  const ids = url.searchParams.get('ids') ?? '';
+
+  const response = await fetch(`${URL}/search.json?q=${query}`, { ...options, method: 'GET' });
+  const data: AutoComplete[] = await response.json();
   const hasData = data.length > 0;
 
   return {
-    hasData,
     status: hasData ? 200 : 404,
-    body: hasData ? parseData(data) : null
-  }
+    body: hasData ? parseData(data, ids) : null,
+  };
 }
