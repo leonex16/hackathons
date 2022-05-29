3fafef1138c6d5ccae7ecf573634cd19dcb3683f
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-29 01:50:25 -0400

    Add weather types

--- weather-app/src/types/index.d.ts
+++ weather-app/src/types/index.d.ts
@@ -2,3 +2,48 @@ export interface Settings {
   isSaveNetworkActive: boolean;
   isDarkModeActive: boolean;
 }
+
+export interface AutocompleteResponse {
+  id:      string;
+  name:    string;
+  region:  string;
+  country: string;
+  src:     string;
+}
+
+interface Condition {
+  icon: string;
+  text: string;
+}
+
+interface Current {
+  dayOrNight:    string;
+  temperature:   Temperature;
+  humidity:      Humidity;
+  precipitation: Humidity;
+  wind:          Humidity;
+}
+
+interface Humidity {
+  label: string;
+  value: number;
+  unit:  string;
+}
+
+interface Temperature {
+  C: string;
+  F: string;
+}
+
+interface Location {
+  name:      string;
+  country:   string;
+  localTime: string;
+}
+
+export interface Weather {
+  id:        string;
+  condition: Condition;
+  location:  Location;
+  current:   Current;
+}
