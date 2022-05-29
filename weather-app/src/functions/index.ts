afc7bd65bd3855530bfd6493831f157b3d6a0044
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-29 15:00:17 -0400

    Check if window element is initialized for use local storage

--- weather-app/src/functions/index.ts
+++ weather-app/src/functions/index.ts
@@ -1,28 +1,30 @@
 export const saveToLocalStorage = (key: string, value: unknown) => {
+  if (globalThis.window === undefined) return;
+
   window.localStorage.setItem(key, JSON.stringify(value));
-}
+};
 
 export const getFromLocalStorage = (key: string) => {
+  if (globalThis.window === undefined) return;
+
   const item = window.localStorage.getItem(key);
-  
-  return item === null
-    ? item
-    : JSON.parse(item);
-}
+
+  return item === null ? item : JSON.parse(item);
+};
 
 export const debounce = (func: (args: Record<string, unknown>) => unknown, waitMs: number) => {
   let debounceTimer = 0;
 
-  return function({ cancel = false, ...args}) {
+  return function ({ cancel = false, ...args }) {
     if (cancel) return;
 
     clearTimeout(debounceTimer);
 
     debounceTimer = window.setTimeout(() => func(args), waitMs);
-  }
-}
+  };
+};
 
-export const handleHttpRequest = async <T>( url: string, options?: RequestInit ): Promise<T> => {
+export const handleHttpRequest = async <T>(url: string, options?: RequestInit): Promise<T> => {
   const response = await fetch(url, options ?? {});
   const data = await response.json();
 
