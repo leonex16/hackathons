d4cde0744487f426ed542cba19d67e56b29cba40
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-29 01:51:13 -0400

    Add Image component

---
+++ weather-app/src/components/image/index.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let className: string;
+  export let src: string;
+  export let alt: string;
+
+  const handleEvent = (evt: Event) => {
+    const img = evt.target as HTMLImageElement;
+    if (evt.type === 'error') {
+      img.src = '/src/static/unknown-country-flag.png';
+    } else if (evt.type === 'load') {
+      img.style.backgroundImage = '';
+    }
+  };
+
+</script>
+
+<img class={className} {src} {alt} on:error={handleEvent} on:load={handleEvent} />
+
+<style>
+  img {
+    background-image: url('../../static/loader-img.svg');
+    background-repeat: no-repeat;
+    background-position: top center;
+  }
+</style>
