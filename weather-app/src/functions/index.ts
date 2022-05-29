d80473f3178b0c077ad1a44ff7f01995be4c4772
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-05-28 23:43:59 -0400

    Add getFromLocalStorage function

--- weather-app/src/functions/index.ts
+++ weather-app/src/functions/index.ts
@@ -1,3 +1,11 @@
 export const saveToLocalStorage = (key: string, value: unknown) => {
   window.localStorage.setItem(key, JSON.stringify(value));
 }
+
+export const getFromLocalStorage = (key: string) => {
+  const item = window.localStorage.getItem(key);
+  
+  return item === null
+    ? item
+    : JSON.parse(item);
+}
