2a5a9a60a9d5ffaa397ed390124380737fc92a84
Leonel Espinoza Sotelo <lespinoza@apside.cl>
2022-06-02 00:08:07 -0400

    Add LoaderScreen component

---
+++ weather-app/src/components/loader-screen/index.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let isLoadResource = true;
+
+  onMount(() => {
+    isLoadResource = !(document.readyState === 'complete');
+  });
+</script>
+
+<div class="loader-screen {isLoadResource || 'loader-screen--hidden'}">
+  <img class="loader-screen__img" src="/loader-img.svg" alt="Loader Screen" />
+</div>
+
+<style>
+  .loader-screen {
+    position: absolute;
+    z-index: 4;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    background-color: var(--surface-variant);
+    overflow: hidden;
+
+    height: 100vh;
+    width: 100vw;
+
+    transition: opacity 400ms ease-in-out,
+                visibility 400ms ease-in-out;
+  }
+
+  .loader-screen--hidden {
+    opacity: 0;
+    visibility: hidden;
+  }
+
+  .loader-screen__img {
+    width: 100%;
+    max-width: 80px;
+    max-height: 100%;
+  }
+</style>
